4806197aba51245c68ca2703b86af8567e32c57e
as3736 <as3736@srmist.edu.in>
2026-04-18 00:57:04 +0530

    Binary Search for Bogie ID (Optimized Searching)

--- App/src/TrainConsistApp.java
+++ App/src/TrainConsistApp.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class TrainConsistApp {
 
     public static void main(String[] args) {
@@ -6,19 +8,31 @@ public class TrainConsistApp {
 
         String[] bogieIds = {"BG101", "BG205", "BG309", "BG412", "BG550"};
 
+        Arrays.sort(bogieIds);
+
         String searchKey = "BG309";
 
+        int left = 0;
+        int right = bogieIds.length - 1;
         boolean found = false;
 
-        for (String id : bogieIds) {
-            if (id.equals(searchKey)) {
+        while (left <= right) {
+            int mid = (left + right) / 2;
+
+            int comparison = bogieIds[mid].compareTo(searchKey);
+
+            if (comparison == 0) {
                 found = true;
                 break;
+            } else if (comparison < 0) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
 
         if (found) {
-            System.out.println("Bogie ID " + searchKey + " found in the train.");
+            System.out.println("Bogie ID " + searchKey + " found using Binary Search.");
         } else {
             System.out.println("Bogie ID " + searchKey + " not found.");
         }
